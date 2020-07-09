f7ec3709747dd29b6e1979b4d6fb73695d1e7a0a
Brianna Keune <briannakeune@gmail.com>
2020-07-09 12:41:12 -0500

    morgan cannot be a dev dependency?

--- api/server.js
+++ api/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 
 const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-if (morgan) server.use(morgan("dev"));
+// if (morgan) server.use(morgan("dev"));
 
 // test route
-server.get("/testing", (req, res) => res.status(200).json({ api: "is up" }));
+server.get("/", (req, res) => res.status(200).json({ api: "is up" }));
 
 module.exports = server;
