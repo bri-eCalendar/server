84a9ed3e2a063b7c1e042cd91dad19ca5a4dbadb
Brianna Keune <briannakeune@gmail.com>
2020-07-09 12:50:26 -0500

    solve deployment issues

--- api/server.js
+++ api/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-// const morgan = require("morgan");
+console.log(process.env.NODE_ENV)
+const morgan = process.env.NODE_ENV === "dev" ? require("morgan") : null;
 
 const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-// if (morgan) server.use(morgan("dev"));
+console.log(morgan)
+if (morgan !== null) server.use(morgan("dev"));
 
 // test route
 server.get("/", (req, res) => res.status(200).json({ api: "is up" }));
